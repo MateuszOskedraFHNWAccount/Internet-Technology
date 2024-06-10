cf942006dd89a55eed6f0f1fe15736b325cb441f
emirhanakguen <130573770+emirhanakguen@users.noreply.github.com>
2024-06-10 18:58:55 +0000

    Quick update of the Application class

--- services/src/main/java/CollaborationApplication.java
+++ services/src/main/java/CollaborationApplication.java
@@ -1,3 +1,25 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import services/src/main/java/business/CollaborationsList.java;
+import services/src/main/java/data/domain/AccountManagement.java;
+import services/src/main/java/data/domain/AdminActions.java;
+import services/src/main/java/data/domain/BuddySystem.java;
+import services/src/main/java/data/domain/ForumPost.java;
+import services/src/main/java/data/domain/ForumResponse.java;
+import services/src/main/java/data/domain/IndependentProject.java;
+import services/src/main/java/data/domain/JobAdvertisement.java;
+import services/src/main/java/data/domain/MentoringForProjects.java;
+import services/src/main/java/data/domain/RoomAssignment.java;
+import services/src/main/java/data/domain/SystemLog.java;
+import services/src/main/java/data/domain/TutoringSession.java;
+import services/src/main/java/data/domain/User.java;
+import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @RestController
 @Hidden // Hide this controller from the Swagger UI
@@ -9,7 +31,7 @@ public class CollaborationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CollaborationApplication.class, args);
 	}
-	
+
 	// Use this method to initialize placeholder data without using Postman
 	// If you are persisting data in a file (see application.properties), initializing data that already exists will cause an error during starting the application
 	// To resolve the error, delete the file and restart the application
