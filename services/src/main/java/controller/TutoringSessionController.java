f6a6de490eb5030043fe92e856af281fe3a88670
emirhanakguen <130573770+emirhanakguen@users.noreply.github.com>
2024-06-10 21:00:40 +0000

    Update of controllers

--- services/src/main/java/controller/TutoringSessionController.java
+++ services/src/main/java/controller/TutoringSessionController.java
@@ -1,5 +1,8 @@
 package services/src/main/java/controller;
 
+import services/src/main/java/business/CollaborationsList.java;
+import services/src/main/java/data/domain/TutoringSession.java;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
